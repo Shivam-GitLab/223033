92767525bbf17a92c176c449319390225506dae0
Shivam-GitLab <shivamkumarsaitm@gmail.com>
2025-04-16 12:42:04 +0530

    Social Media Api Test

---
+++ Social Media/src/main/java/com/spring/socialmedia/model/Post.java
@@ -0,0 +1,16 @@
+package com.spring.socialmedia.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    private int id;
+    private int userid;
+    private String content;
+}
